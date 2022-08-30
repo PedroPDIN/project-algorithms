e54c60b106b16f5f5554ef7572783d6b58c4de79
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-30 20:33:06 -0300

    fix(requisito-3): alteração de metodos

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,15 +1,4 @@
-def bubble_sort_first_string(array):
-    size_array = len(array)
-    for element in range(size_array - 1):
-        for item in range(0, size_array - 1 - element):
-            if array[item] > array[item + 1]:
-                current_element = array[item]
-                array[item] = array[item + 1]
-                array[item + 1] = current_element
-    return "".join(array).lower()
-
-
-def bubble_sort_second_string(array):
+def bubble_sort_string(array):
     size_array = len(array)
     for element in range(size_array - 1):
         for item in range(0, size_array - 1 - element):
@@ -21,8 +10,8 @@ def bubble_sort_second_string(array):
 
 
 def is_anagram(first_string, second_string):
-    current_first_string = bubble_sort_first_string(list(first_string))
-    current_second_string = bubble_sort_second_string(list(second_string))
+    current_first_string = bubble_sort_string(list(first_string))
+    current_second_string = bubble_sort_string(list(second_string))
     print(current_first_string)
 
     return current_first_string == current_second_string
