c1600142d76860e20e2211237616985f84eb2ac8
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-30 18:21:31 -0300

    feat(requisito-2): adiciona requisito completo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    if len(word) < 1:
+        return False
+
+    size_word = len(word[low_index:high_index])
+    if size_word < 1:
+        return True
+    else:
+        return word[low_index] == word[high_index] and is_palindrome_recursive(
+          word, low_index + 1, high_index - 1
+        )
