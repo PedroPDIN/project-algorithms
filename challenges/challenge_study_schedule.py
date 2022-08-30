51bcfbed800461ad2f051d66d214878377aeb0fa
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-30 17:08:55 -0300

    feat(requisito-1): adiciona requisito completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    size_period = len(permanence_period)
+    count_period = 0
+
+    for i in range(0, size_period):
+        first = permanence_period[i][0]
+        second = permanence_period[i][1]
+        try:
+            if first <= target_time <= second:
+                count_period += 1
+        except TypeError:
+            return None
+
+    return count_period
